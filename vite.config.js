43885eca203a0b244d75b25bd4306f6699b99612
HanaaMohammed11 <hanaamohammed840@gmail.com>
2024-10-07 04:09:12 -0700

    edit

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,17 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // يمكنك تغيير المنفذ حسب الحاجة
+    open: true, // لفتح المتصفح تلقائياً عند بدء التشغيل
+  },
+  build: {
+    outDir: 'dist', // الدليل الناتج بعد البناء
+  },
   css: {
     preprocessorOptions: {
       css: {
-        additionalData: '@import "./src/index.css";' // تأكد من استيراد ملف CSS الخاص بك
+        additionalData: '@import "./src/index.css";' 
       },
     },
   },
