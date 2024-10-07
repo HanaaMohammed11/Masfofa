c0e6122531534e18f94a5f1d83394bed82045f24
HanaaMohammed11 <hanaamohammed840@gmail.com>
2024-10-07 01:01:53 -0700

    edit

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import Ar_Lang from './Locals/Ar/Common.json';
 import En_Lang from './Locals/En/Common.json';
 import { I18nextProvider } from 'react-i18next';
 import { TranslateProvider } from './TranslateContext/TransContext.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 i18next.init({
   interpolation: {
@@ -24,10 +25,14 @@ i18next.init({
 });
 
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
     <I18nextProvider i18n={i18next}>
       <TranslateProvider>
-      <App />
+        < BrowserRouter>
+        <App />
+        </BrowserRouter> 
+    
       </TranslateProvider>
   
     </I18nextProvider>
