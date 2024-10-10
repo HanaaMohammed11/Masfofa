8e2615847605b14ccefebe765ab77d0445457f4f
HanaaMohammed11 <hanaamohammed840@gmail.com>
2024-10-10 00:36:42 -0700

    edit

--- src/pages/Home/Card.jsx
+++ src/pages/Home/Card.jsx
@@ -11,7 +11,7 @@ import "./Card.css";
 export default function Cards() {
   const { t } = useTranslation("global");
   const [user, setUser] = useState("");
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -37,82 +37,82 @@ export default function Cards() {
 
   return (
     <div className="flex justify-center items-center min-h-96 mt-44 mb-44">
-  <div className="flex gap-4 items-center sm:p-4 flex-wrap justify-center"> 
-    {/* Card 1 */}
-    <div className="p-4 text-center relative bg-transparent w-80"> 
-      <img
-        src={Planet}
-        alt="Spinning Planet"
-        className="inset-0 w-full h-full object-contain"
-        style={{ animation: "spin 10s linear infinite" }}
-      />
-      <button
-        className="aux-button aux-medium aux-carmine-pink aux-curve aux-none aux-uppercase"
-        onClick={() => navigate("/users")}
-      >
-        <h1 className="aux-text" >
-          {t("text.Employees")}
-        </h1>
-      </button>
-    </div>
+      <div className="flex gap-4 items-center sm:p-4 flex-wrap justify-center">
+        
+        {/* Card 1 */}
+        <div className="p-4 text-center relative bg-transparent w-72 card-container card-1"> 
+          <img
+            src={Planet}
+            alt="Spinning Planet"
+            className="inset-0 w-full h-full object-contain"
+            style={{ animation: "spin 10s linear infinite" }}
+          />
+          <button
+            className="aux-button aux-medium aux-carmine-pink aux-curve aux-none aux-uppercase"
+            onClick={() => navigate("/users")}
+          >
+            <h1 className="aux-text">
+              {t("text.Employees")}
+            </h1>
+          </button>
+        </div>
 
-    {/* Card 2 */}
-    <div className="p-4 text-center relative bg-transparent w-80"> 
-      <img
-        src={Planet}
-        alt="Spinning Planet"
-        className="inset-0 w-full h-full object-contain"
-        style={{ animation: "spin 10s linear infinite" }}
-      />
-      <button
-        className="aux-button aux-medium aux-carmine-pink aux-curve aux-none aux-uppercase"
-        onClick={() => navigate("/sujects")}
-      >
-        <h1 className="aux-text" >
-          {t("text.Articles")}
-        </h1>
-      </button>
-    </div>
+        {/* Card 2 */}
+        <div className="p-4 text-center relative bg-transparent w-72 card-container card-2"> 
+          <img
+            src={Planet}
+            alt="Spinning Planet"
+            className="inset-0 w-full h-full object-contain"
+            style={{ animation: "spin 10s linear infinite" }}
+          />
+          <button
+            className="aux-button aux-medium aux-carmine-pink aux-curve aux-none aux-uppercase"
+            onClick={() => navigate("/sujects")}
+          >
+            <h1 className="aux-text">
+              {t("text.Articles")}
+            </h1>
+          </button>
+        </div>
 
-    {/* Card 3 */}
-    <div className="p-4 text-center relative bg-transparent w-80"> 
-      <img
-        src={Planet}
-        alt="Spinning Planet"
-        className="inset-0 w-full h-full object-contain"
-        style={{ animation: "spin 10s linear infinite" }}
-      />
-      <button
-        className="aux-button aux-medium aux-carmine-pink aux-curve aux-none aux-uppercase"
-        onClick={() => navigate("/Matrix")}
-      >
-        <h1 className="aux-text" >
-          {t("text.Matrices")}
-        </h1>
-      </button>
-    </div>
+        {/* Card 3 */}
+        <div className="p-4 text-center relative bg-transparent w-72 card-container card-3"> 
+          <img
+            src={Planet}
+            alt="Spinning Planet"
+            className="inset-0 w-full h-full object-contain"
+            style={{ animation: "spin 10s linear infinite" }}
+          />
+          <button
+            className="aux-button aux-medium aux-carmine-pink aux-curve aux-none aux-uppercase"
+            onClick={() => navigate("/Matrix")}
+          >
+            <h1 className="aux-text">
+              {t("text.Matrices")}
+            </h1>
+          </button>
+        </div>
 
-    {/* Card 4 */}
-    {user.accountType === "admin" && (
-      <div className="p-4 text-center relative bg-transparent w-80"> 
-        <img
-          src={Planet}
-          alt="Spinning Planet"
-          className="inset-0 w-full h-full object-contain"
-          style={{ animation: "spin 10s linear infinite" }}
-        />
-        <button
-          className="aux-button aux-medium aux-carmine-pink aux-curve aux-none aux-uppercase"
-          onClick={() => navigate("/dashboard")}
-        >
-          <h1 className="aux-text" >
-            {t("text.DashBoard")}
-          </h1>
-        </button>
+        {/* Card 4 (Visible for admin only) */}
+        {user.accountType === "admin" && (
+          <div className="p-4 text-center relative bg-transparent w-72 card-container card-4"> 
+            <img
+              src={Planet}
+              alt="Spinning Planet"
+              className="inset-0 w-full h-full object-contain"
+              style={{ animation: "spin 10s linear infinite" }}
+            />
+            <button
+              className="aux-button aux-medium aux-carmine-pink aux-curve aux-none aux-uppercase"
+              onClick={() => navigate("/dashboard")}
+            >
+              <h1 className="aux-text">
+                {t("text.DashBoard")}
+              </h1>
+            </button>
+          </div>
+        )}
       </div>
-    )}
-  </div>
-</div>
-
+    </div>
   );
 }
