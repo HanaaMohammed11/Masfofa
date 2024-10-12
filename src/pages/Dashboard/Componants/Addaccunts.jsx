5563ace1f73e1e8c32933a3dbb525fc45669b79b
YoussefFathy6 <yousseffathy20@gmail.com>
2024-10-12 16:48:55 +0300

    accounts form added

--- src/pages/Dashboard/Componants/Addaccunts.jsx
+++ src/pages/Dashboard/Componants/Addaccunts.jsx
@@ -19,6 +19,8 @@ import { AiFillDelete, AiFillEdit } from "react-icons/ai";
 
 emailjs.init("vRSobHxRYCwqKML2w");
 
+// Other imports remain the same
+
 export default function AddAccounts() {
   const { t, i18n } = useTranslation("global");
   const direction = i18n.language === "ar" ? "rtl" : "ltr";
@@ -30,12 +32,19 @@ export default function AddAccounts() {
   const usersCollection = collection(db, "users");
   const auth = getAuth();
   const [user, setUser] = useState([]);
+
+  // Provide a default value for accountType
   const initialValues = {
     firstName: "",
     lastName: "",
     email: "",
     password: "",
-    accountType: "employee",
+    accountType:
+      user.length > 0
+        ? user[0].accountType === "superAdmin"
+          ? "admin"
+          : "employee"
+        : "employee", // Default to "employee"
   };
 
   const validationSchema = Yup.object().shape({
@@ -153,7 +162,7 @@ export default function AddAccounts() {
               placeholder={t("addaccount.searchPlaceholder")}
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
-              className="block w-full px-4 py-2  rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              className="block w-full px-4 py-2 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
             />
           </div>
 
@@ -173,70 +182,172 @@ export default function AddAccounts() {
               >
                 {({ isSubmitting }) => (
                   <Form>
-                    {/* Form fields here */}
-                    <div className="w-full">
-                      <Button type="submit" disabled={isSubmitting}>
-                        {isSubmitting
-                          ? t("addaccount.registering")
-                          : t("addaccount.register")}
-                      </Button>
+                    <div className="space-y-6" dir={direction}>
+                      <h3 className="text-xl font-medium text-gray-900 dark:text-white">
+                        {t("addaccount.createAccount")}
+                      </h3>
+
+                      {error && <div className="text-red-500">{error}</div>}
+
+                      <div>
+                        <div className="mb-2 block">
+                          <Label
+                            htmlFor="firstName"
+                            value={t("addaccount.firstName")}
+                          />
+                        </div>
+                        <Field
+                          name="firstName"
+                          type="text"
+                          as={TextInput}
+                          id="firstName"
+                          placeholder={t("addaccount.firstName")}
+                        />
+                        <ErrorMessage
+                          name="firstName"
+                          component="div"
+                          className="text-red-500 text-sm"
+                        />
+                      </div>
+
+                      <div>
+                        <div className="mb-2 block">
+                          <Label
+                            htmlFor="lastName"
+                            value={t("addaccount.lastName")}
+                          />
+                        </div>
+                        <Field
+                          name="lastName"
+                          type="text"
+                          as={TextInput}
+                          id="lastName"
+                          placeholder={t("addaccount.lastName")}
+                        />
+                        <ErrorMessage
+                          name="lastName"
+                          component="div"
+                          className="text-red-500 text-sm"
+                        />
+                      </div>
+
+                      <div>
+                        <div className="mb-2 block">
+                          <Label
+                            htmlFor="email"
+                            value={t("addaccount.email")}
+                          />
+                        </div>
+                        <Field
+                          name="email"
+                          type="email"
+                          as={TextInput}
+                          id="email"
+                          placeholder="name@company.com"
+                        />
+                        <ErrorMessage
+                          name="email"
+                          component="div"
+                          className="text-red-500 text-sm"
+                        />
+                      </div>
+
+                      <div>
+                        <div className="mb-2 block">
+                          <Label
+                            htmlFor="password"
+                            value={t("addaccount.password")}
+                          />
+                        </div>
+                        <Field
+                          name="password"
+                          type="password"
+                          as={TextInput}
+                          id="password"
+                          placeholder="••••••••"
+                        />
+                        <ErrorMessage
+                          name="password"
+                          component="div"
+                          className="text-red-500 text-sm"
+                        />
+                      </div>
+                      <div>
+                        <div className="mb-2 block">
+                          <Label
+                            htmlFor="accountType"
+                            value={t("addaccount.accType")}
+                          />
+                        </div>
+                        <Field
+                          as="select"
+                          name="accountType"
+                          id="accountType"
+                          className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                        >
+                          {user.length > 0 &&
+                            user[0].accountType === "superAdmin" && (
+                              <option value="admin">
+                                {t("addaccount.superAdmin")}
+                              </option>
+                            )}
+                          <option value="admin">{t("addaccount.admin")}</option>
+                          <option value="employee">
+                            {t("addaccount.emp")}
+                          </option>
+                        </Field>
+                        <ErrorMessage
+                          name="accountType"
+                          component="div"
+                          className="text-red-500 text-sm"
+                        />
+                      </div>
+                      <div className="w-full">
+                        <Button type="submit" disabled={isSubmitting}>
+                          {isSubmitting
+                            ? t("addaccount.loading")
+                            : t("addaccount.create")}
+                        </Button>
+                      </div>
                     </div>
                   </Form>
                 )}
               </Formik>
             </Modal.Body>
           </Modal>
+        </div>
 
-          {/* Employees Table */}
-          <div
-            className="overflow-x-auto flex flex-col items-center"
-            dir={direction}
-          >
-            <div
-              dir={direction}
-              className="overflow-x-auto w-full mx-auto p-4 rounded-lg shadow-lg mt-10"
-            >
-              <table
-                dir="rtl"
-                className="table-auto min-w-full bg-[#D3A17A] text-sm md:text-base"
-              >
-                <thead dir={direction}>
-                  <tr dir={direction} className="bg-[#D3A17A] text-white">
-                    <th className="px-4 py-2">{t("addaccount.firstName")}</th>
-                    <th className="px-4 py-2">{t("addaccount.email")}</th>
-                    <th className="px-4 py-2">{t("addaccount.password")}</th>
-                    <th className="px-4 py-2">{t("addaccount.accType")}</th>
-                    <th className="px-4 py-2">{t("subjectInfo.action")}</th>
-                  </tr>
-                </thead>
-
-                <tbody className="text-gray-700">
-                  {filteredEmployees.length > 0 ? (
-                    filteredEmployees.map((employee) => (
-                      <tr
-                        key={employee.ID}
-                        className="border-t hover:bg-gray-100"
-                      >
-                        <td className="px-4 py-2">
-                          {employee.firstname} {employee.lastname}
-                        </td>
-                        <td className="px-4 py-2">{employee.email}</td>
-                        <td className="px-4 py-2">{employee.password}</td>
-                        <td className="px-4 py-2">{employee.accountType}</td>
-                        <td className="px-4 py-2 flex justify-center space-x-2"></td>
-                      </tr>
-                    ))
-                  ) : (
-                    <tr>
-                      <td colSpan="5" className="px-4 py-10 text-center">
-                        {t("addaccount.noUsers")}
-                      </td>
-                    </tr>
-                  )}
-                </tbody>
-              </table>
-            </div>
-          </div>
+        {/* Employee List */}
+        <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  {t("addaccount.name")}
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  {t("addaccount.email")}
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  {t("addaccount.action")}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredEmployees.map((employee) => (
+                <tr
+                  key={employee.docId}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                >
+                  <td className="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                    {employee.firstname} {employee.lastname}
+                  </td>
+                  <td className="px-6 py-4">{employee.email}</td>
+                  <td className="px-6 py-4"></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
